8e82320df40090ac36ffd74c32597200703b6786
Amjad Elahi <amjadelahi07@gmail.com>
2024-09-28 14:45:19 +0300

    add Poppins font

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,13 @@
 import 'package:device_preview/device_preview.dart';
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
+import 'package:onze_cofe_project/screens/welcome_screen/employee/orders_screen.dart';
 import 'package:onze_cofe_project/screens/welcome_screen/welcome_screen.dart';
-import 'package:onze_cofe_project/setup/setup_init.dart';
 import 'package:supabase_flutter/supabase_flutter.dart';
 
-void main() async {
+import 'setup/setup_init.dart';
+
+void main() async{
   WidgetsFlutterBinding.ensureInitialized();
 
   await Supabase.initialize(
@@ -14,9 +16,10 @@ void main() async {
         "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImF0bHhtamxsdGZpa3VjYWl6b2VnIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MjczNzkyNDMsImV4cCI6MjA0Mjk1NTI0M30.tdRENvJ39QV3yoTiEDTagQr7xQ3k-sHENQxwGGEuvF8",
   );
   setup();
+
   runApp(DevicePreview(
     enabled: !kReleaseMode,
-    builder: (context) => MainApp(), // Wrap your app
+    builder: (context) => const MainApp(),
   ));
 }
 
@@ -25,8 +28,9 @@ class MainApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: WelcomeScreen(),
+    return MaterialApp(
+      theme: ThemeData(fontFamily: 'Poppins'),
+      home: const OrdersScreen(),
     );
   }
 }
